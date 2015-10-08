f9189760c2091855250d6d4ac9d59c0f1f7bd107
michaltaberski <m.taberski@gmail.com>
2015-10-08 22:58:41 +0200

    add simple js watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var plumber = require('gulp-plumber');
 
 gulp.task('default', function () {
-    return gulp.src('src/app.js')
-        .pipe(babel())
-        .pipe(gulp.dest('dist'));
+  return gulp.src('src/app.js')
+    .pipe(plumber())
+    .pipe(babel())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve', function() {
+  gulp.watch(['src/**/*.js'], ['default']);
 });
